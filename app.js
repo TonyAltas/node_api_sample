76cb45c9a26e7bb97726758b691b287b19a83e52
Tony <TonyAltas@gmail.com>
2017-02-24 00:11:59 +0000

    Add endpoints that interact with mongodb

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var contacts = require('./routes/contacts');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,36 +2,83 @@ var express = require('express');
 var router = express.Router();
 var url = require('url');
 var contacts = require('../modules/contacts');
+var mongoose = require('mongoose');
+var dataservice = require('../modules/contactdataservice');
+
+mongoose.connect('mongodb://localhost/contacts');
+
+
+var contactSchema = new mongoose.Schema({
+	primarycontactnumber: {type: String, index: {unique:
+	true}},
+	 firstname: String,
+	 lastname: String,
+	 title: String,
+	 company: String,
+	 jobtitle: String,
+	 othercontactnumbers: [String],
+	 primaryemailaddress: String,
+	 emailaddresses: [String],
+	 groups: [String]
+});
+var Contact = mongoose.model('Contact', contactSchema);
+
+
 
 /* GET home page. */
-router.get('/contacts', function(req, res, next) {
-	var get_params = url.parse(req.url, true).query;
-	if (Object.keys(get_params).length === 0){
-		res.setHeader('content-type', 'application/json');
-		res.end(JSON.stringify(contacts.list()));
-	}
-	else {
-		res.setHeader('content-type', 'application/json');
-		res.end(JSON.stringify(contacts.query_by_arg(get_params.arg,
-					get_params.value)));
-	}
+//router.get('/contacts', function(req, res, next) {
+//	var get_params = url.parse(req.url, true).query;
+//	if (Object.keys(get_params).length === 0){
+//		res.setHeader('content-type', 'application/json');
+//		res.end(JSON.stringify(contacts.list()));
+//	}
+//	else {
+//		res.setHeader('content-type', 'application/json');
+//		res.end(JSON.stringify(contacts.query_by_arg(get_params.arg,
+//					get_params.value)));
+//	}
+//});
+
+router.get('/contacts', function(request, response) {
+	console.log('Listing all contacts with ' +
+		request.params.key + '=' + request.params.value);
+	dataservice.list(Contact, response);
+});
+router.get('/contacts/:number', function(request, response) {
+	console.log(request.url + ' : querying for ' +
+		request.params.number);
+	dataservice.findByNumber(Contact, request.params.number,
+		response);
+});
+router.post('/contacts', function(request, response) {
+	dataservice.update(Contact, request.body, response)
+});
+router.put('/contacts', function(request, response) {
+	dataservice.create(Contact, request.body, response)
+});
+router.delete('/contacts/:primarycontactnumber', function(request, response) {
+	dataservice.remove(Contact,
+		request.params.primarycontactnumber, response);
 });
 
-router.get('/contacts/:number', function(req, res) {
-	res.setHeader('content-type', 'application/json');
-	res.end(JSON.stringify(contacts.query(req.params.number)));
-});
 
-router.get('/groups', function(req, res) {
-	console.log('groups');
-	res.setHeader('content-type', 'application/json');
-	res.end(JSON.stringify(contacts.list_groups()));
-});
 
-router.get('/groups/:name', function(req, res){
-	console.log('groups');
-	res.setHeader('content-type', 'application/json');
-	res.end(JSON.stringify(contacts.get_members(req.params.name)));
-});
+
+//router.get('/contacts/:number', function(req, res) {
+//	res.setHeader('content-type', 'application/json');
+//	res.end(JSON.stringify(contacts.query(req.params.number)));
+//});
+//
+//router.get('/groups', function(req, res) {
+//	console.log('groups');
+//	res.setHeader('content-type', 'application/json');
+//	res.end(JSON.stringify(contacts.list_groups()));
+//});
+//
+//router.get('/groups/:name', function(req, res){
+//	console.log('groups');
+//	res.setHeader('content-type', 'application/json');
+//	res.end(JSON.stringify(contacts.get_members(req.params.name)));
+//});
 
 module.exports = router;
